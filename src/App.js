84f84ee6d1c697ea85064ffb311d7db2181e22a0
gilsun <gilsun0821@gmail.com>
2021-12-28 08:20:04 -0800

    email and password form with react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  // const sendEmial = (e) => {
+  //   e.preventDefault();
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form>
+          <div className="row pt-5 mx-auto">
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input type="text" className="form-control" placeholder="Name" />
+            </div>
+
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="password"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <Button variant="primary"> submit</Button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
